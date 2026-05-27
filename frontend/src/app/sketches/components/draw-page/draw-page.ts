69598fcf187865b3be653fe50bfee3515206dd47
Enrico11678 <enricoajese11@gmail.com>
2026-05-27 22:28:31 +0200

    Aggiustato resizing per la tavolozza

--- frontend/src/app/sketches/components/draw-page/draw-page.ts
+++ frontend/src/app/sketches/components/draw-page/draw-page.ts
@@ -143,9 +143,13 @@ export class DrawPage implements OnInit, OnDestroy {
   private getCanvasCoordinates(e: MouseEvent) {
     const canvas = this._canvasRef.nativeElement;
     const rect = canvas.getBoundingClientRect();
+    
+    const scaleX = canvas.width / rect.width;
+    const scaleY = canvas.height / rect.height;
+
     return {
-      x: e.clientX - rect.left,
-      y: e.clientY - rect.top
+      x: (e.clientX - rect.left) * scaleX,
+      y: (e.clientY - rect.top) * scaleY
     };
   }
 
